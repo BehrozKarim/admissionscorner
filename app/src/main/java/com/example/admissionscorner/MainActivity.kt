844b62b5695cdc1104f73e6e3d6d66ecb4fa348c
BehrozKarim <behrozbuzdar@gmail.com>
2023-08-13 01:43:08 +0500

    feat(webview): added a webview to display website

--- app/src/main/java/com/example/admissionscorner/MainActivity.kt
+++ app/src/main/java/com/example/admissionscorner/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.admissionscorner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("http://admissionscorner.com/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
     }
 }
